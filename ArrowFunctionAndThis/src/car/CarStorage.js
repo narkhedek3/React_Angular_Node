b7146aaf37dc27e6c1484da2ec721cc43267845e
narkhedek3 <narkhedek3@gmail.com>
2020-04-12 17:45:52 +0530

    12 april 2020 5:45PM

--- ArrowFunctionAndThis/src/car/CarStorage.js
+++ ArrowFunctionAndThis/src/car/CarStorage.js
@@ -1,6 +1,6 @@
 
 class CarStorage {
-  
+
   constructor(cars) {
     this.cars = cars || [];
   }
@@ -10,25 +10,21 @@ class CarStorage {
   }
 
   removeCar(carId) {
-    const carIndex = this.cars.findIndex(function(car) {
-      return car.id === carId;
-    });
+    const carIndex = this.cars.findIndex(car => car.id === carId);
 
-    if(carIndex === -1) {
+    if (carIndex === -1) {
       console.log('car does not exist...');
       return;
     }
-    this.cars.splice(carIndex,1);
+    this.cars.splice(carIndex, 1);
   }
 
   listCars() {
-    if(this.cars.length === 0) {
+    if (this.cars.length === 0) {
       console.log('No cars in the garage..');
       return;
     }
-    this.cars.forEach(function(car) {
-      car.displayCarInfo(); 
-    })
+    this.cars.forEach(car => car.displayCarInfo())
   }
 
 }
