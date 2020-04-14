7b95b3da37f808de9a64e8a09ea20d92866762ad
narkhedek3 <narkhedek3@gmail.com>
2020-04-14 19:01:56 +0530

    14.4.2020 7:00PM

---
+++ React_Samples/first-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="make-center fullscreen make-bold">
+        My First React App....!!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
